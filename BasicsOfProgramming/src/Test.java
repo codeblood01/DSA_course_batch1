2403acb24c10adbb78affafb5eef93dd81a63ea2
codeblood01 <gouravbhatt744@gmail.com>
2022-06-13 20:14:51 +0530

    test

--- BasicsOfProgramming/src/Test.java
+++
@@ -1,9 +0,0 @@
-
-class Test{
-    public static void main(String[] args) {
-        System.out.println("check");
-        Check af = new Check();
-        af.a=5;
-        System.out.println(af.a);
-    }
-}
