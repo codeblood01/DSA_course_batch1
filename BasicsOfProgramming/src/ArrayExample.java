3266df065ff36dfed739d67e16c5bd1b7b95f626
codeblood01 <gouravbhatt744@gmail.com>
2022-06-13 22:59:01 +0530

    pattern

---
+++ BasicsOfProgramming/src/ArrayExample.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ArrayExample {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static char[][] patternCreator(){
+        char result[][] = new char[4][5];
+        for (int i = 0; i < 4; i++) {
+            if(i==0){
+                result[i][0]='*';
+                break;
+            }
+            for (int j = 0; j < i+1; j++) {
+                if(j==0||j==i+1||i==4){
+                    result[i][j]='*';
+                }else{
+                    result[i][j]='^';
+                }
+            }
+        }
+        return result;
+    }
+
+}
